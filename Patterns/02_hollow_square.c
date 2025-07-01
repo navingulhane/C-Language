274a353f36e69c111a1c98640eeffc3b5f88b2ed
navingulhane <navingulhane007@gmail.com>
2025-07-01 11:01:11 +0530

    Patterns/02_hollow_square.c

---
+++ Patterns/02_hollow_square.c
@@ -0,0 +1,23 @@
+// Hollow Square ka codeee [^__^]
+  #include <stdio.h>
+
+  int main() {
+  int n, i, j;
+
+  printf("Enter the size of the square: ");
+  scanf("%d", &n);
+
+  for(i = 1; i <= n; i++) {
+  for(j = 1; j <= n; j++) {
+// Borders pr star ke liye :) 
+  if(i == 1 || i == n || j == 1 || j == n) {
+  printf("* ");
+  } else {
+  printf("  ");
+// For space inside the sphere ^_^ 
+  }
+  }
+  printf("\n");
+  }
+  return 0;
+  }
