c07db869640c04a4a78d38261c8273069405726d
navingulhane <navingulhane007@gmail.com>
2025-07-01 11:03:53 +0530

    03_hollow_triangle.c

---
+++ Patterns/03_hollow_triangle.c
@@ -0,0 +1,22 @@
+ // HOLLOW TRIANGLE [^__^]
+    #include <stdio.h>
+    int main ()  {
+    int i , j , n;
+    
+    printf("Enter the number of rows\n");
+    printf("==> ");
+    scanf("%d", &n);
+    
+    for(i = 1; i <= n; i++) {
+    for(j = 1; j <= i; j++) {
+  // Print star[ * ]in the beginning ^_^ 
+    if(j == i|| j == 1 || i == n ) 
+    printf("* ");
+    else
+  // Print space where [ j != i&1 ]; [ i != n ] ^_^ 
+    printf("  ");
+    }
+    printf("\n");
+    }
+    return 0;
+    }
