6bea2b7ba9ef6cc1c515af7a2c2c74f9d2bc6070
navingulhane <navingulhane007@gmail.com>
2025-07-02 21:51:05 +0530

    Create hello_world.c

---
+++ 01_Basics/hello_world.c
@@ -0,0 +1,8 @@
+//  hello_world.c – My first C program. Just prints a simple message. Beginning of my coding journey ^ _ ^
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Panda is learning C!\n");
+    return 0;
+}
