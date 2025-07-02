b7ef4303fb23113466ea03ddbfa4ddf22d3c6de2
navingulhane <navingulhane007@gmail.com>
2025-07-02 22:16:11 +0530

    Create sum_of_two_numbers.c in basics folder ^__^

---
+++ 01_Basics/sum_of_two_numbers.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+ int main() {
+ int num1 , num2 , sum;
+ 
+ printf("Entre the first number : ");
+ scanf("%d", &num1);
+ 
+ printf("Entre the secound number : ");
+ scanf("%d", &num2);
+ 
+ sum = num1 + num2;
+ printf("The sum is\n");
+ printf("===> %d", sum);
+ return 0;
+ }
