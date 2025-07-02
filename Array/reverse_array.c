cb4e7266253eab2649dd6f3480fe2224f2ea0ea4
navingulhane <navingulhane007@gmail.com>
2025-07-02 21:27:12 +0530

    Create reverse_array.c in Array folder

---
+++ Array/reverse_array.c
@@ -0,0 +1,17 @@
+    #include <stdio.h>
+    
+    int main () {
+    int arr[5];
+    int i ; 
+    
+    printf("Entre 5 numbers:\n");
+    for(i = 0; i < 5; i++ ) {
+    scanf("%d", & arr[i] );
+    }
+    printf("Reversed order is : \n");
+    for( i = 4; i >= 0; i--) {
+    printf("%d\n", arr[i]);
+    }
+    
+    return 0;
+    }
