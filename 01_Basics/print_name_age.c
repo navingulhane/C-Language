284c903feeefcb89ccc410b605cac9193aef1789
navingulhane <navingulhane007@gmail.com>
2025-07-03 17:22:57 +0530

    Create print_name_age.c

---
+++ 01_Basics/print_name_age.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+    char name [] = "Navin gulhane";
+    int age = 18 ;
+    
+    printf("NAme: %s\n", name);
+    printf("Age : %d\n", age);
+    
+    return 0;
+} 
