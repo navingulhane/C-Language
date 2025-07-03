5891980dedc28ed481f7b35e1f3ef9fe14d8e90b
navingulhane <navingulhane007@gmail.com>
2025-07-03 17:33:27 +0530

    Create variables_datatypes.c

---
+++ 01_Basics/variables_datatypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ( ) {
+    int age = 18 ;
+    float weight = 84.5 ;
+    char initial = 'N';
+    char name [] = "Navin" ;
+    
+    printf("age : %d \n" , age);
+    printf("weight : %.2f\n",weight);
+    printf("initial : %c \n",initial);
+    printf("name : %s\n",name);
+    
+    return 0;
+}
