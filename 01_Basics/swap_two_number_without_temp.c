025fd3aac5c2a8a813f0ca95fde81a80b1ecc17b
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-04 23:37:31 +0530

    Create swap_two_number_without_temp.c

---
+++ 01_Basics/swap_two_number_without_temp.c
@@ -0,0 +1,19 @@
+ // Online C compiler to run C program online
+  #include <stdio.h>
+  int main () {
+  int a , b ;
+  printf("Enter two numbers :\n");
+  scanf("%d %d" , &a , &b);
+    
+ // Swapping without a temp
+ 
+  a = a + b;
+  b = a - b;
+  a = a - b;
+ 
+  printf("After swapping :\n");
+  printf("a = %d\n",a);
+  printf("b = %d\n",b);
+ 
+  return 0;
+  }   
