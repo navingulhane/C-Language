859795111f0c94ef08d6817cbead653b41053cd1
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-04 12:19:00 +0530

    Create swap_with_temp.c

---
+++ 01_Basics/swap_with_temp.c
@@ -0,0 +1,24 @@
+// Swapping of two numbers ^___^
+  
+    #include <stdio.h>
+
+    int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers:\n");
+    printf("==> ");
+    scanf("%d %d", &a, &b);
+
+    printf("\n< == == == RESULT == == == >\n");
+    printf("\nBefore swapping: a = %d, b = %d\n", a, b);
+
+ // Swapping logic... [^o^]
+ 
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+    }
