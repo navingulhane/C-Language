34d312422eb56e38077e13ae311fc0effc8515ca
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-04 11:39:56 +0530

    Create arithmetic_operations.c

---
+++ 01_Basics/arithmetic_operations.c
@@ -0,0 +1,24 @@
+ // doing arithmetic operations with two numbers 
+
+     #include <stdio.h>
+     int main ( )  {
+   
+     int a , b ; 
+   
+     printf("Entre two number :\n");
+     scanf("%d  %d" , &a , &b);
+     
+     printf("\n---Result---\n");
+     printf("Addition :%d\n" , a+b);
+     printf("Subtraction :%d\n" , a-b);
+     printf("Multiplication :%d\n", a*b);
+    
+  //for division there is special case a loop is used for it [If Else loop] ^_^
+    
+    if(b != 0) {
+        printf("Division :%f\n", (float) a/b );
+    }
+    else {
+        printf("Dividion not possible [b = 0]\n");
+    }
+    }
