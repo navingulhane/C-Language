951f7663edbe7ca1fc9e99d1a9f1165e05e7ab1f
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-05 21:36:22 +0530

    sum_of_first_N_natural_numbers.c

---
+++ 01_Basics/sum_of_first_N_natural_numbers.c
@@ -0,0 +1,14 @@
+   #include <stdio.h>
+   int main() {
+       
+   int n, sum;
+   
+   printf("Enter a positive integer");
+   scanf("%d", &n);
+  
+   sum = n * (n+1)/2;
+   
+   printf("The sum of first %d natural no is:%d\n", n,sum);
+   
+   return 0;
+   }
