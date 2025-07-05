f50c30e8c1de64ff8a1467a3e66654acd7cec81c
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-05 11:03:23 +0530

    Create even_odd.c

---
+++ 01_Basics/even_odd.c
@@ -0,0 +1,19 @@
+// EVEN / ODD (^0^)
+   #include <stdio.h>
+
+   int main() {
+   int num ;
+   
+   printf("Entre a number :");
+   scanf("%d", &num);
+   
+   if (num %2 == 0) {
+// make sure that num/2 gives 0 remender ^_^       
+   printf("%d is Even a number\n", num);
+   }
+   else {
+   printf("%d is Odd a number\n", num);
+   }
+
+   return 0;
+   }
