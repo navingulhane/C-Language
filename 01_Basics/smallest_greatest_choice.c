6cdbafc262d69dcce11d2f4b0cd2b7a3a47da1e8
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-05 17:14:49 +0530

    smallest_greatest_choice.c

---
+++ 01_Basics/smallest_greatest_choice.c
@@ -0,0 +1,36 @@
+        #include <stdio.h>
+        int main() {
+        int a, b, c, choice;
+    
+        printf("Entre three numbers :\n");
+        scanf("%d %d %d", &a, &b, &c);
+    // choice logic ^=^
+        printf("\nEnter 1 for Smallest, 2 for Greatest\n");
+        scanf("%d", &choice);
+    
+        if(choice == 1) {
+            //smallest
+        if (a <= b && a <= c) {
+            printf("%d is the smallest number\n",a);
+        }
+        else if(b <= a && b <= c) {   
+        printf("%d  is the smallest number\n",b);    
+        }
+        else {
+        printf("%d is the smallest number\n",c);
+        }
+        }
+        else if (choice == 2) {
+            //greatest
+        if(a >= b && a >= c) {
+            printf("%d is the greatest number\n",a);
+        }
+        else if (b >= a && b >= c) {
+            printf("%d is the greatest number\n",b);
+        }
+        else {
+            printf("%d is the greatest number\n",c);
+        }
+        }
+        return 0;
+        }
