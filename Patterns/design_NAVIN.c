819d7df0834a67651b3acba4ba3572917cd882fd
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-08 21:52:44 +0530

    Create design_NAVIN.c

---
+++ Patterns/design_NAVIN.c
@@ -0,0 +1,23 @@
+// Diagonal letter pattern for name "Navin" ^_^
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[] = "Navin";
+    int len = strlen(name);
+
+    for(int i = 0; i < len; i++) {
+        printf("|");
+        for(int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        printf("%c", name[i]);
+        for(int j = i + 1; j < len; j++) {
+            printf(" ");
+        }
+        printf("|\n");
+    }
+
+    return 0;
+}
