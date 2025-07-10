4e9fba48fd10d5055aa5bfe84aab6dbc9f201d8c
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-10 20:50:33 +0530

    Update Theory_for_Arithmetic_instructions.c

--- 01_Basics/Arithmetic_instructions.c
+++ 01_Basics/Arithmetic_instructions.c
@@ -1,1 +1,6 @@
-
+It is consist of Operand and Operator
+Operand`s are like(a,b,x,y,z....etc)
+Operator`s are like (+,-,/,*)
+EXAMPLE ==> sum = a + b
+Note: Single variable on the left hand side [LHS]
+^_^
