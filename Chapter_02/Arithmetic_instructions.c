1874f410c8776d0b383b6ad538abf93530b90823
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-12 20:52:37 +0530

    Update Arithmetic_instructions.c

--- Chapter_02/Arithmetic_instructions.c
+++ Chapter_02/Arithmetic_instructions.c
@@ -1,1 +1,10 @@
-
+It is consist of Operand and Operator
+Operand`s are like(a,b,x,y,z....etc)
+Operator`s are like (+,-,/,*)
+EXAMPLE ==> sum = a + b
+Note: Single variable on the left hand side [LHS]
+^_^
+**MODULAR OPERATOR** [ % }
+  a % b = Remainder 
+  Mostly used in calculators (can see my mini cal project (^ _ ^)
+  EXAMPLE ==> 4 % 2 = 0 and 5 % 3 = 2
