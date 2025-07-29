8db22924b020601db90728957babb4bb231b8ae9
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-29 20:49:31 +0530

    Update and rename Data_types to Data_types.c

---
+++ 001_CHAPTERS_in_c_languge/Chapter 1 /Data_types.c
@@ -0,0 +1,9 @@
+It genrally means that how much byte doest any specific words in C contains
+
+Signed and Unsigned 'char' ==> 1 Byte
+
+Signed and Unsigned 'int and short-int' ==> 2 Byte
+
+Signed and Unsigned 'long int' ==> 4 byte
+
+< ========================================================================= >
