b23bdf55507b3a0fbba7421b364d015c33097d7c
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-29 20:45:01 +0530

    Update and rename Input to Input.c

---
+++ 001_CHAPTERS_in_c_languge/Chapter 1 /Input.c
@@ -0,0 +1,7 @@
+Lets directly take some 'cases' for better understanding :
+
+(1) scanf("%d",&age);
+(2) scanf("%f",&pi);
+(3) scanf("%c",&star);
+
+< ========================================================= >
