a0dad05885e90b3e5e45428e0d255e078dc1775d
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-29 20:33:40 +0530

    Update variables.c

--- 001_CHAPTERS_in_c_languge/Chapter 1 /variables.c
+++ 001_CHAPTERS_in_c_languge/Chapter 1 /variables.c
@@ -1,1 +1,8 @@
 Name of memory location which stores some data in it are known as VARIABLES ^_^
+
+There are some rules :
+(i) Case sensitive
+(ii) First letter must be ALPHABET or '_'
+(iii) There should be no ',' or space ' ' between words 
+
+< ============================================================================= >
