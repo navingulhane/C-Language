377e3a5807b89ea8bc05e8f42f00c7ffb4edf6fb
billu_badmosh ;) <navingulhane007@gmail.com>
2025-07-29 20:55:18 +0530

    Create Comments.c

---
+++ 001_CHAPTERS_in_c_languge/Chapter 1 /Comments.c
@@ -0,0 +1,12 @@
+Lines which are not a part of code are know as ' Comments '
+
+There are 2 types in comments 
+
+(1)  //  For single lien ^_^         
+
+(2)  /*    for 
+           multiple 
+           lines ^_^    
+     */
+
+  < ================================================================ >
