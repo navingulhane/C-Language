23b327893f90df06f0b48c21c964726a70658b73
Dogesh_badmosh ;) <navingulhane007@gmail.com>
2025-08-03 19:21:34 +0530

    Create 3_number_sum.c

---
+++ just_Practice/3_number_sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main () {
+int a,b,c,sum;
+printf("Enter 3 number\n");
+printf("==> ");
+scanf("%d %d %d",&a , &b,&c);
+sum = a + b + c;
+printf("the sum is : %d\n",sum);
+return 0;
+}
