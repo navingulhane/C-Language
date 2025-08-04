db300b1d3f2869f885db4f5bbd5ea55e07881743
Dogesh_badmosh ;) <navingulhane007@gmail.com>
2025-08-04 20:35:41 +0530

    Create parallelogram.c

---
+++ just_Practice/parallelogram.c
@@ -0,0 +1,22 @@
+// parallelogram ka code ^_^
+  #include <stdio.h>
+
+  int main() {
+  int i, j, n;
+
+  printf("Enter the number of rows: ");
+  scanf("%d", &n);
+
+  for(i = 1; i <= n; i++) {
+// Print spaces first to shift stars
+  for(j = 1; j <= n - i; j++) {
+  printf(" ");
+  }
+// for printing star [ * ]
+  for(j = 1; j <= n; j++) {
+  printf("* ");
+  }
+  printf("\n");
+  }
+  return 0;
+  }
