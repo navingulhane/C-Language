d1f2ff10fd20b9c2973bdfb808bd7ad4884d8e05
Dogesh_badmosh ;) <navingulhane007@gmail.com>
2025-08-04 20:32:51 +0530

    Create greatest_of_3_number.c

---
+++ just_Practice/greatest_of_3_number.c
@@ -0,0 +1,20 @@
+// Check the greatest of 3 numbers
+
+#include <stdio.h>
+
+int main() {
+int a, b, c;
+printf("Enter 3 numbers:\n");
+scanf("%d %d %d", &a, &b, &c);
+
+if (a >= b && a >= c) {
+printf("%d is the greatest number\n", a);
+}
+else if (b >= a && b >= c) {
+printf("%d is the greatest number\n", b);
+}
+else {
+printf("%d is the greatest number\n", c);
+}
+return 0;
+}
