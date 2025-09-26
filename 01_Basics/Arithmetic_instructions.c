1ef09fd74224da7204934c5ff33c0574df1bbd25
Dogesh_badmosh ;) <navingulhane007@gmail.com>
2025-09-26 23:19:02 +0530

    Arithmetic_instructions.c

--- 01_Basics/Arithmetic_instructions.c
+++ 01_Basics/Arithmetic_instructions.c
@@ -3,7 +3,7 @@ Operand`s are like(a,b,x,y,z....etc)
 Operator`s are like (+,-,/,*)
 EXAMPLE ==> sum = a + b.
 Note: Single variable on the left hand side [LHS]
-^_^
+^_^..
 **MODULAR OPERATOR** [ % }
   a % b = Remainder 
   Mostly used in calculators (can see my mini cal project (^ _ ^)
