ed6b914d475784c40e331e91fb6178986d4da469
---navin <navingulhane007@gmail.com>
2025-11-19 20:52:30 +0530

    Create sum-of_digit_of_any_number.c

---
+++ just_Practice/sum-of_digit_of_any_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    int main () {
+    int num ,digit ,sum = 0;
+    
+    printf("Enter any number :");
+    scanf("%d",&num);
+  
+    for( ; num > 0 ; num = num / 10 ) {
+        
+        digit = num % 10 ;
+        
+        sum = sum + digit;
+    }
+        printf("%d",sum);
+    
+}
