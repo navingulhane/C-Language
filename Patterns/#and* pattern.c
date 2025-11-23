545ea3b37751d6dc6e1f8c900656ee29a3820e50
---navin <navingulhane007@gmail.com>
2025-11-23 17:18:30 +0530

    Create #and* pattern.c

---
+++ Patterns/#and* pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+
+    for ( int i = 1; i <= 4; i++){
+        printf(" #");
+    }
+    printf(" \n");
+
+    for( int i = 1; i <= 4; i ++){
+        printf(" *");
+    }
+    return 0;
+}
+
+
+ # # # # 
+ * * * *
